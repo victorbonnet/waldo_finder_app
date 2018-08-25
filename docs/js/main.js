2654c61c6a4d136da070e87695f400b3f7dc579c
Victor Bonnet <victor.bonnet@gmail.com>
2018-08-26 01:18:13 +0200

    Add camera template from https://github.com/kasperkamperman/MobileCameraTemplate

---
+++ docs/js/main.js
@@ -0,0 +1,311 @@
+/*
+
+>> kasperkamperman.com - 2018-04-18
+>> https://www.kasperkamperman.com/blog/camera-template/
+
+*/
+
+var takeSnapshotUI = createClickFeedbackUI();
+
+var video;
+var takePhotoButton;
+var toggleFullScreenButton;
+var switchCameraButton;
+var amountOfCameras = 0;
+var currentFacingMode = 'environment';
+
+const GOOGLE_CLOUD_STORAGE_DIR = 'https://raw.githubusercontent.com/victorbonnet/waldo_finder_app/master/docs/web_model/';
+const MODEL_URL = GOOGLE_CLOUD_STORAGE_DIR + 'tensorflowjs_model.pb';
+const WEIGHTS_URL = GOOGLE_CLOUD_STORAGE_DIR + 'weights_manifest.json';
+
+let modelPromise;
+
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    // do some WebRTC checks before creating the interface
+    DetectRTC.load(function() {
+
+        // do some checks
+        if (DetectRTC.isWebRTCSupported == false) {
+            alert('Please use Chrome, Firefox, iOS 11, Android 5 or higher, Safari 11 or higher');
+        }
+        else {
+            if (DetectRTC.hasWebcam == false) {
+                alert('Please install an external webcam device.');
+            }
+            else {
+
+                amountOfCameras = DetectRTC.videoInputDevices.length;
+                       
+                initCameraUI();
+                initCameraStream();
+            } 
+        }
+        
+        console.log("RTC Debug info: " + 
+            "\n OS:                   " + DetectRTC.osName + " " + DetectRTC.osVersion + 
+            "\n browser:              " + DetectRTC.browser.fullVersion + " " + DetectRTC.browser.name +
+            "\n is Mobile Device:     " + DetectRTC.isMobileDevice +
+            "\n has webcam:           " + DetectRTC.hasWebcam + 
+            "\n has permission:       " + DetectRTC.isWebsiteHasWebcamPermission +       
+            "\n getUserMedia Support: " + DetectRTC.isGetUserMediaSupported + 
+            "\n isWebRTC Supported:   " + DetectRTC.isWebRTCSupported + 
+            "\n WebAudio Supported:   " + DetectRTC.isAudioContextSupported +
+            "\n is Mobile Device:     " + DetectRTC.isMobileDevice
+        );
+
+    });
+
+    modelPromise = tf.loadFrozenModel(MODEL_URL, WEIGHTS_URL);
+
+});
+
+function initCameraUI() {
+    
+    video = document.getElementById('video');
+
+    takePhotoButton = document.getElementById('takePhotoButton');
+    toggleFullScreenButton = document.getElementById('toggleFullScreenButton');
+    switchCameraButton = document.getElementById('switchCameraButton');
+    
+    // https://developer.mozilla.org/nl/docs/Web/HTML/Element/button
+    // https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/ARIA_Techniques/Using_the_button_role
+
+    takePhotoButton.addEventListener("click", function() {
+        takeSnapshotUI();
+        takeSnapshot();        
+    });
+
+    // -- fullscreen part
+
+    function fullScreenChange() {
+        if(screenfull.isFullscreen) {
+            toggleFullScreenButton.setAttribute("aria-pressed", true);
+        }
+        else {
+            toggleFullScreenButton.setAttribute("aria-pressed", false);
+        }
+    }
+
+    if (screenfull.enabled) {
+        screenfull.on('change', fullScreenChange);
+
+        toggleFullScreenButton.style.display = 'block';  
+
+        // set init values
+        fullScreenChange();
+
+        toggleFullScreenButton.addEventListener("click", function() {
+            screenfull.toggle(document.getElementById('container'));
+        });
+    }
+    else {
+        console.log("iOS doesn't support fullscreen (yet)");   
+    }
+        
+    // -- switch camera part
+    if(amountOfCameras > 1) {
+        
+        switchCameraButton.style.display = 'block';
+        
+        switchCameraButton.addEventListener("click", function() {
+
+            if(currentFacingMode === 'environment') currentFacingMode = 'user';
+            else                                    currentFacingMode = 'environment';
+
+            initCameraStream();
+
+        });  
+    }
+
+    // Listen for orientation changes to make sure buttons stay at the side of the 
+    // physical (and virtual) buttons (opposite of camera) most of the layout change is done by CSS media queries
+    // https://www.sitepoint.com/introducing-screen-orientation-api/
+    // https://developer.mozilla.org/en-US/docs/Web/API/Screen/orientation
+    window.addEventListener("orientationchange", function() {
+        
+        // iOS doesn't have screen.orientation, so fallback to window.orientation.
+        // screen.orientation will 
+        if(screen.orientation) angle = screen.orientation.angle;
+        else                   angle = window.orientation;
+
+        var guiControls = document.getElementById("gui_controls").classList;
+        var vidContainer = document.getElementById("vid_container").classList;
+
+        if(angle == 270 || angle == -90) {
+            guiControls.add('left');
+            vidContainer.add('left');
+        }
+        else {
+            if ( guiControls.contains('left') ) guiControls.remove('left');
+            if ( vidContainer.contains('left') ) vidContainer.remove('left');
+        }
+
+        //0   portrait-primary   
+        //180 portrait-secondary device is down under
+        //90  landscape-primary  buttons at the right
+        //270 landscape-secondary buttons at the left
+    }, false);
+    
+}
+
+// https://github.com/webrtc/samples/blob/gh-pages/src/content/devices/input-output/js/main.js
+function initCameraStream() {
+
+    // stop any active streams in the window
+    if (window.stream) {
+        window.stream.getTracks().forEach(function(track) {
+            track.stop();
+        });
+    }
+
+    var constraints = { 
+        audio: false, 
+        video: {
+            //width: { min: 1024, ideal: window.innerWidth, max: 1920 },
+            //height: { min: 776, ideal: window.innerHeight, max: 1080 },
+            // width: {exact: 480},
+            // height: {exact: 640},
+            facingMode: currentFacingMode
+        }
+    };
+
+    navigator.mediaDevices.getUserMedia(constraints).
+    then(handleSuccess).catch(handleError);   
+
+    function handleSuccess(stream) {
+
+        window.stream = stream; // make stream available to browser console
+        video.srcObject = stream;
+
+        if(constraints.video.facingMode) {
+
+            if(constraints.video.facingMode === 'environment') {
+                switchCameraButton.setAttribute("aria-pressed", true);
+            }
+            else {
+                switchCameraButton.setAttribute("aria-pressed", false);
+            }
+        }
+
+        return navigator.mediaDevices.enumerateDevices();
+    }
+
+    function handleError(error) {
+
+        console.log(error);
+
+        //https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+        if(error === 'PermissionDeniedError') {
+            alert("Permission denied. Please refresh and give permission.");
+        }
+        
+    }
+
+}
+
+function takeSnapshot() {
+    
+    // if you'd like to show the canvas add it to the DOM
+    // var canvas = document.createElement('canvas');
+    var canvas = document.getElementById('canvas');
+
+    var width = video.videoWidth;
+    var height = video.videoHeight;
+
+    canvas.width = width;
+    canvas.height = height;
+
+    context = canvas.getContext('2d');
+    context.drawImage(video, 0, 0, width, height);
+
+    // polyfil if needed https://github.com/blueimp/JavaScript-Canvas-to-Blob
+    
+    // https://developers.google.com/web/fundamentals/primers/promises
+    // https://stackoverflow.com/questions/42458849/access-blob-value-outside-of-canvas-toblob-async-function
+    function getCanvasBlob(canvas) {
+        return new Promise(function(resolve, reject) {
+            canvas.toBlob(function(blob) { resolve(blob) }, 'image/jpeg');
+        })
+    }
+
+    // some API's (like Azure Custom Vision) need a blob with image data
+    getCanvasBlob(canvas).then(function(blob) {
+        startInference();
+    });
+
+    async function startInference() {
+      const model = await modelPromise;
+
+      const pixels = tf.fromPixels(canvas).toFloat().expandDims(0);
+      console.log('model loaded');
+      console.time('predict');
+
+      const res = await model.executeAsync(pixels);
+      console.timeEnd('predict');
+      const boxes = await res[0].data();
+      const scores = await res[1].data();
+      // const classes = await res[2].data();
+      // const numDetect = await res[3].data();
+
+      console.log(boxes)
+      console.log(scores)
+      // console.log(classes)
+      // console.log(numDetect)
+
+      scores.forEach(function(score, index) {
+        if (score > 0.98) {
+          const top = boxes[index];
+          const left = boxes[index+1];
+          const bottom = boxes[index+2];
+          const right = boxes[index+3];
+
+          const minY = top * canvas.height;
+          const minX = left * canvas.width;
+          const maxY = bottom * canvas.height;
+          const maxX = right * canvas.width;
+
+          context.beginPath();
+          context.rect(minX, minY, maxX - minX, maxY - minY);
+          context.lineWidth = 2;
+          context.strokeStyle = 'blue';
+          context.stroke();
+        }
+      });
+    }
+
+}
+
+// https://hackernoon.com/how-to-use-javascript-closures-with-confidence-85cd1f841a6b
+// closure; store this in a variable and call the variable as function
+// eg. var takeSnapshotUI = createClickFeedbackUI();
+// takeSnapshotUI();
+
+function createClickFeedbackUI() {
+
+    // in order to give feedback that we actually pressed a button. 
+    // we trigger a almost black overlay
+    var overlay = document.getElementById("video_overlay");//.style.display;
+
+    // sound feedback
+    var sndClick = new Howl({ src: ['snd/click.mp3'] });
+
+    var overlayVisibility = false;
+    var timeOut = 5000;
+
+    function setFalseAgain() {
+        overlayVisibility = false;	
+        overlay.style.display = 'none';
+    }
+
+    return function() {
+
+        if(overlayVisibility == false) {
+            sndClick.play();
+            overlayVisibility = true;
+            overlay.style.display = 'block';
+            setTimeout(setFalseAgain, timeOut);
+        }   
+
+    }
+}
